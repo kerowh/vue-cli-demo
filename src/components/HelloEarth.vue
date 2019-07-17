194cbfe6dbd1549cd1b462fcadda6acef95cbb31
keo <380213949@qq.com>
2019-07-17 21:27:42 +0800

    使用vue-cli进行项目的搭建

---
+++ src/components/HelloEarth.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <h2>{{$route.params.earthmsg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'HelloEarth'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: red;
+}
+
+</style>
---
+++ src/components/choose.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="choose">
+      <ul>
+        <li>
+          <router-link :to="{name:'HelloWorld',params:{worldmsg:'你好世界'}}">HelloWorld</router-link>
+        </li>
+        <li>
+          <router-link :to="{name:'HelloEarth',params:{earthmsg:'你好地球'}}">HelloEarth</router-link>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'choose',
+  worldmsg: '',
+  earthmsg: ''
+}
+</script>
+
+<style scoped>
+
+</style>
